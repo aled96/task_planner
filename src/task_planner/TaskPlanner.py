9827d5e4483ddbe949ba22bfbb0e8e8a73f62f03
aled96 <aled96@hotmail.it>
2024-10-21 16:06:04 +0200

    Small extension

--- src/task_planner/TaskPlanner.py
+++ src/task_planner/TaskPlanner.py
@@ -3,68 +3,111 @@ from unified_planning.shortcuts import *
 
 class TaskPlanner:
     def __init__(self):
+        self.setTypes()
+        self.setFluents()
+        self.setActions()
+        self.setProblem()
+        self.addObjects()
+        self.setInitState()
+        self.setGoalState()
+
+    def setTypes(self):
         #Types
-        Obj = UserType('Obj')
-        #Tool = UserType('Tool')
-        Robot = UserType('Robot')
+        self.Obj = UserType('Obj')
+        self.Tool = UserType('Tool', father = self.Obj)
+        self.Robot = UserType('Robot')
 
+    def setFluents(self):
         #Fluents
-        grasped = unified_planning.model.Fluent('grasped', BoolType(), r=Robot, o=Obj)
-        is_tight = unified_planning.model.Fluent('is_tight', BoolType(), o=Obj)
-        free_hands = unified_planning.model.Fluent('free_hands', BoolType(), r=Robot)
-        can_tight = unified_planning.model.Fluent('can_tight', BoolType(), o=Obj)
-
-        #Actions
+        self.grasped = unified_planning.model.Fluent('grasped', BoolType(), r=self.Robot, o=self.Obj)
+        self.is_tight = unified_planning.model.Fluent('is_tight', BoolType(), o=self.Obj)
+        self.requires_tool = unified_planning.model.Fluent('requires_tool', BoolType(), o=self.Obj, t= self.Tool)
+        self.is_turnable = unified_planning.model.Fluent('is_turnable', BoolType(), o=self.Obj)
+        self.free_hands = unified_planning.model.Fluent('free_hands', BoolType(), r=self.Robot)
+        self.can_tight = unified_planning.model.Fluent('can_tight', BoolType(), o=self.Obj)
+        
+    def setActions(self):
         #Grasp
-        grasp = InstantaneousAction('grasp', r=Robot, o=Obj)
-        r = grasp.parameter('r')
-        o = grasp.parameter('o')
+        self.grasp = InstantaneousAction('grasp', r=self.Robot, o=self.Obj)
+        r = self.grasp.parameter('r')
+        o = self.grasp.parameter('o')
 
-        grasp.add_precondition(free_hands(r))
-        grasp.add_precondition(Not(grasped(r, o)))
+        self.grasp.add_precondition(self.free_hands(r))
+        self.grasp.add_precondition(Not(self.grasped(r, o)))
 
-        grasp.add_effect(grasped(r, o), True)
-        grasp.add_effect(free_hands(r), False)
+        self.grasp.add_effect(self.grasped(r, o), True)
+        self.grasp.add_effect(self.free_hands(r), False)
+
+        #Put Down
+        self.put_down = InstantaneousAction('put_down', r=self.Robot, o=self.Obj)
+        r = self.put_down.parameter('r')
+        o = self.put_down.parameter('o')
+
+        self.put_down.add_precondition(Not(self.free_hands(r)))
+        self.put_down.add_precondition(self.grasped(r, o))
+
+        self.put_down.add_effect(self.grasped(r, o), False)
+        self.put_down.add_effect(self.free_hands(r), True)
+
 
         #Tight
-        tight = InstantaneousAction('tight', r=Robot, t=Obj, o=Obj)
-        r = tight.parameter('r')
-        t = tight.parameter('t')
-        o = tight.parameter('o')
+        self.tight = InstantaneousAction('tight', r=self.Robot, t=self.Tool, o=self.Obj)
+        r = self.tight.parameter('r')
+        t = self.tight.parameter('t')
+        o = self.tight.parameter('o')
 
-        tight.add_precondition(can_tight(t))
-        tight.add_precondition(grasped(r, t))
-        tight.add_precondition(Not(is_tight(o)))
+        self.tight.add_precondition(self.can_tight(t))
+        self.tight.add_precondition(self.grasped(r, t))
+        self.tight.add_precondition(self.requires_tool(o, t))
+        self.tight.add_precondition(Not(self.is_tight(o)))
 
-        tight.add_effect(is_tight(o), True)
+        self.tight.add_effect(self.is_tight(o), True)
 
-        #Problem
+        #Turn
+        self.turn = InstantaneousAction('turn', r=self.Robot, o=self.Obj)
+        r = self.turn.parameter('r')
+        o = self.turn.parameter('o')
+
+        self.turn.add_precondition(self.is_turnable(o))
+        self.turn.add_precondition(self.grasped(r, o))
+        self.turn.add_precondition(Not(self.is_tight(o)))
+
+        self.turn.add_effect(self.is_tight(o), True)
+        
+
+    def setProblem(self):
         self.problem = Problem('Problem')
-        self.problem.add_fluent(grasped, default_initial_value=False)
-        self.problem.add_fluent(is_tight, default_initial_value=False)
-        self.problem.add_fluent(free_hands, default_initial_value=False)
-        self.problem.add_fluent(can_tight, default_initial_value=False)
+        self.problem.add_fluent(self.grasped, default_initial_value=False)
+        self.problem.add_fluent(self.is_tight, default_initial_value=False)
+        self.problem.add_fluent(self.free_hands, default_initial_value=False)
+        self.problem.add_fluent(self.can_tight, default_initial_value=False)
+        self.problem.add_fluent(self.is_turnable, default_initial_value=False)
+        self.problem.add_fluent(self.requires_tool, default_initial_value=False)
 
-        self.problem.add_action(grasp)
-        self.problem.add_action(tight)
+        self.problem.add_action(self.grasp)
+        self.problem.add_action(self.put_down)
+        self.problem.add_action(self.tight)
+        self.problem.add_action(self.turn)
 
-        #Add Objects
-        Objects = [Object("Centauro", Robot),
-                Object("Screwdriver", Obj),
-                Object("Screw", Obj)]
+    def addObjects(self):
+        self.Objects = [Object("Centauro", self.Robot),
+                        Object("Screwdriver", self.Tool),
+                        Object("Screw", self.Obj),
+                        Object("Valve", self.Obj)]
 
-        self.problem.add_objects(Objects)
+        self.problem.add_objects(self.Objects)
 
-        #Initial State
-        self.problem.set_initial_value(is_tight(Objects[2]), False)
-        self.problem.set_initial_value(free_hands(Objects[0]), True)
-        self.problem.set_initial_value(can_tight(Objects[1]), True)
+    def setInitState(self):
+        self.problem.set_initial_value(self.is_tight(self.Objects[2]), False)
+        self.problem.set_initial_value(self.free_hands(self.Objects[0]), True)
+        self.problem.set_initial_value(self.can_tight(self.Objects[1]), True)
+        self.problem.set_initial_value(self.is_turnable(self.Objects[3]), True)
+        self.problem.set_initial_value(self.requires_tool(self.Objects[2], self.Objects[1]), True)
 
-        #Add goal
-        self.problem.add_goal(is_tight(Objects[2]))
-        #problem.add_goal(grasped(Objects[0], Objects[1]))
+    def setGoalState(self):
+        self.problem.add_goal(self.is_tight(self.Objects[2]))
+        self.problem.add_goal(self.is_tight(self.Objects[3]))
 
-        # print(self.problem)
 
     def solve(self):
         with OneshotPlanner(problem_kind=self.problem.kind) as planner:
